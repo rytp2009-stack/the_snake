ba7e5b72352c269b8a60cae4878952568b91efba
Илья Федоров <rytp2009@gmail.com>
2026-04-23 22:50:48 +0300

    добавлен класс Apple

--- the_snake.py
+++ the_snake.py
@@ -60,6 +60,35 @@ class GameObject:
         Абстрактный метод для отрисовки объекта на экране.
         Аргумент: surface(поверхность, на которой рисуем)
         """
+class Apple(GameObject):
+    """
+    Класс Apple. Наследуется от GameObject.
+    Появляется в случайном месте поля.
+    """
+    apple_color=(255,0,0)
+    super().__init__(position=None,body_color=apple_color)
+    self.randomize_position()
+    def randomize_position(self):
+        """
+        Устанавливает случайные координаты для яблока.
+        """
+        max_x=640-20
+        max_y=480-20
+        x.random.randrage(0,max_x+1,20)
+        y.random.randrage(0,max_y+1,20)
+        self.position=(x, y)
+    def draw(self, surface):
+        """
+        Отрисовывает яблоко на игровом поле.
+        """
+        rect=pygame.Rect(
+            self.position[0],
+            self.position[1],
+            20,
+            20
+        )
+        pygame.draw.rect(surface,self.body_color,rect)
+
 
 
 def main():
